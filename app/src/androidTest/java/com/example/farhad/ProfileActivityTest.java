7faf7f6abd41fb68d68b9844b55d2105fd1ffef3
Farhad Bahrehmand <43734894+farhadbahrehmandhenry@users.noreply.github.com>
2020-05-01 17:28:22 -0700

    c

--- app/src/androidTest/java/com/example/farhad/ProfileActivityTest.java
+++ app/src/androidTest/java/com/example/farhad/ProfileActivityTest.java
@@ -7,19 +7,50 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 public class ProfileActivityTest {
     @Rule
-    public ActivityTestRule<SignupActivity> activityTestRule = new ActivityTestRule<>(SignupActivity.class);
+    public ActivityTestRule<ProfileActivity> activityTestRule = new ActivityTestRule<>(ProfileActivity.class);
 
     @Test
     public void goesToMain() {
         onView(withId(R.id.backButton)).perform(click());
 
-        SignupActivity activity = activityTestRule.getActivity();
+        ProfileActivity activity = activityTestRule.getActivity();
 
         assertTrue(activity.isFinishing());
+    }
+
+    @Test
+    public void validatesOccupation() {
+        onView(withId(R.id.occupation)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.nextButton)).perform(click());
+
+        ProfileActivity activity = activityTestRule.getActivity();
+        onView(withText("occupation is required")).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void validatesDescription() {
+        onView(withId(R.id.occupation)).perform(typeText("programmer"), closeSoftKeyboard());
+        onView(withId(R.id.description)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.nextButton)).perform(click());
+
+        ProfileActivity activity = activityTestRule.getActivity();
+        onView(withText("description is required")).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
     }
 }
